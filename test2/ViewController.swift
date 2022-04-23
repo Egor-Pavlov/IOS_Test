d78690b345eee491b4c4263f8c0e204443d62381
VitChernuhin <vitaliy.chernukhin@eltex.loc>
2022-04-23 16:21:18 +0700

    началась война с клавиатурой

--- test2/ViewController.swift
+++ test2/ViewController.swift
@@ -6,13 +6,17 @@ import UIKit
 //самый нижний слой
 class ViewController: UIViewController
 {
-    let ExitButton = UIButton()
+    let DialogButton = UIButton()
     let label2 = UILabel()
+    let newView = UIView()
+    let buttonlabel = UILabel()
+    
     @objc func ExitButtonClicked(_ sender: Any)
     {
-        
-        let a = DialogListViewController()
+        //создаем новое окно
+        let a = DialogViewController()
         let b:UIViewController = a as UIViewController
+        //добавляем окно в стек окон
         navigationController?.pushViewController(b, animated: true)
 
     }
@@ -21,36 +25,20 @@ class ViewController: UIViewController
         super.viewDidAppear(animated)
         super.viewDidLoad()
         print(navigationController!.viewControllers.count)
-        //navigationController?.accessibilityElements?.removeAll()
-//        let date = Date()
-//        let calendar = Calendar.current
-//        let hour = calendar.component(.hour, from: date)
-//        let minute = calendar.component(.minute, from: date)
-//        let second = calendar.component(.second, from: date)
-//        label2.text = String(hour) + " " + String (minute) + " " + String(second)
-        
+    
     }
     override func viewDidLoad()
     {
         
         super.viewDidLoad()
-        let date = Date()
-        let calendar = Calendar.current
-        let hour = calendar.component(.hour, from: date)
-        let minute = calendar.component(.minute, from: date)
-        let second = calendar.component(.second, from: date)
 
-      
-        //1. Создаём объект
-        let newView = UIView()
+        //Устанавливаем ему цвет и другие параметры
+        newView.backgroundColor = UIColor.systemGray6
 
-        //2. Устанавливаем ему цвет и другие параметры
-        newView.backgroundColor = UIColor.systemBackground
-
-        //3. Добавляем его (newView) на наше основное View
+        //Добавляем его на основное View (чтобы полностью перекрасить)
         view.addSubview(newView)
 
-        //4. Отключаем constraint перед установкой своих
+        //Отключаем constraint перед установкой своих
         newView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate(
             [
@@ -58,48 +46,29 @@ class ViewController: UIViewController
                 newView.topAnchor.constraint(equalTo: view.topAnchor),
                 newView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
                 newView.leftAnchor.constraint(equalTo: view.leftAnchor)
-
-            ]
-        )
-        newView.addSubview(label2)
-
-        label2.textAlignment = .center
-        label2.text = String(hour) + " " + String (minute) + " " + String(second)
-        label2.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate(
-            [
-                label2.rightAnchor.constraint(equalTo: label2.superview!.safeAreaLayoutGuide.rightAnchor, constant: 0),
-                label2.topAnchor.constraint(equalTo: label2.superview!.safeAreaLayoutGuide.topAnchor, constant: 70),
-                label2.bottomAnchor.constraint(equalTo: label2.superview!.safeAreaLayoutGuide.topAnchor, constant: 100),
-                label2.leftAnchor.constraint(equalTo: label2.superview!.safeAreaLayoutGuide.leftAnchor, constant: 0)
-
             ]
         )
         
-        ExitButton.backgroundColor = UIColor.systemBackground
-        ExitButton.titleLabel?.textColor = UIColor.blue
-//        ExitButton.layer.borderColor = UIColor.gray.cgColor
-//        ExitButton.layer.borderWidth = 1
-    
-        
-        let buttonText = "Exit"
+        let buttonText = "Диалог №1"
         let font = UIFont.systemFont(ofSize: 25)
         let attributes = [NSAttributedString.Key.font: font]
         let attributeString = NSAttributedString(string: buttonText, attributes: attributes)
-        ExitButton.setAttributedTitle(attributeString, for: .normal)
-                //button.setTitle(" Button", forState: .Normal)
-
-        ExitButton.addTarget(self, action: #selector(ExitButtonClicked), for: .touchUpInside)
-        ExitButton.translatesAutoresizingMaskIntoConstraints = false
-        newView.addSubview(ExitButton)
+        DialogButton.setAttributedTitle(attributeString, for: .normal)
+        DialogButton.backgroundColor = UIColor.systemBackground
+        DialogButton.addTarget(self, action: #selector(ExitButtonClicked), for: .touchUpInside)
+        DialogButton.translatesAutoresizingMaskIntoConstraints = false
+                
+        newView.addSubview(DialogButton)
     
         NSLayoutConstraint.activate(
         [
-            ExitButton.rightAnchor.constraint(equalTo: ExitButton.superview!.safeAreaLayoutGuide.rightAnchor, constant: 0),
-            ExitButton.topAnchor.constraint(equalTo: ExitButton.superview!.safeAreaLayoutGuide.topAnchor, constant: 0),
-            ExitButton.bottomAnchor.constraint(equalTo: ExitButton.superview!.safeAreaLayoutGuide.topAnchor, constant: 50),
-            ExitButton.leftAnchor.constraint(equalTo: ExitButton.superview!.safeAreaLayoutGuide.leftAnchor, constant: 0)
+            DialogButton.rightAnchor.constraint(equalTo: DialogButton.superview!.safeAreaLayoutGuide.rightAnchor, constant: 0),
+            DialogButton.topAnchor.constraint(equalTo: DialogButton.superview!.safeAreaLayoutGuide.topAnchor, constant: 0),
+            DialogButton.bottomAnchor.constraint(equalTo: DialogButton.superview!.safeAreaLayoutGuide.topAnchor, constant: 50),
+            DialogButton.leftAnchor.constraint(equalTo: DialogButton.superview!.safeAreaLayoutGuide.leftAnchor, constant: 0)
          
         ])
+        
+
     }
 }
